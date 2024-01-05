e5b5e6b8344effb57b1b0d1d1807c2514f2be056
cjberson <cjberson@gmail.com>
2024-01-04 21:45:48 -0500

    cleaned up the code

--- player.gd
+++ player.gd
@@ -16,6 +16,37 @@ const SIN_45 = sin(PI / 4)
 
 
 func _physics_process(delta):	
+	set_target_velocity(delta)
+	handle_stacking()
+	
+	if base == null:
+		velocity = target_velocity
+		move_and_slide()
+	else:
+		base.velocity = target_velocity
+		base.move_and_slide()
+		
+		# BUG: Setting position of player directly makes it so their collider is ignored
+		# Place player directly above the stacked NPC
+		position.x = base.position.x
+		position.y = base.position.y + 1
+		position.z = base.position.z
+		
+func set_target_velocity(delta: float) -> void:
+	var direction = get_direction()
+	
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Gravity
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	jump_or_throw()	
+	target_velocity = cartesian_to_isometric(target_velocity)	
+		
+func get_direction() -> Vector3:
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
@@ -35,70 +66,59 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
 	
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
-	if not is_on_floor():	 # If in the air, fall towards the floor
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-	
+	return direction
+		
+		
+func jump_or_throw() -> void:	
 	if Input.is_action_just_pressed("jump"):
+		# Jumping
 		if base == null:
-			# Jumping
+			# Only jump if the player is on the floor
 			if is_on_floor(): 
 				target_velocity.y = jump_impulse
 		# Throwing
 		else:
+			# TODO: change to launch impulse rather than jump-esque
+			# TODO: once thrown, try allowing them to land back on same npc
 			target_velocity.y = jump_impulse
+			# After the player is thrown, do not let them land back onto the same NPC
 			prev_base = base
 			base = null
 		
-	# Iterate through all collisions that occurred this frame
-	for index in range(get_slide_collision_count()):
-		# We get one of the collisions with the player
-		var collision = get_slide_collision(index)
-		var collider = collision.get_collider()
-		
-		# If the collision is with ground or the base
-		if collider == null or collider == base or collider == prev_base:
-			continue
-
-		if collider.is_in_group("world"):
-			if Vector3.UP.dot(collision.get_normal()) > 0.1:
-				prev_base = null
-				
-				break
-			
-		# If the collider is with an npc
-		if collider.is_in_group("npc"):
-			# we check that we are hitting it from above.
-			if Vector3.UP.dot(collision.get_normal()) > 0.1:
-				position.x = collider.position.x
-				position.y = collider.position.y + 1
-				position.z = collider.position.z
-				base = collider
-				
-				break		
-	print(target_velocity)
-	target_velocity = cartesian_to_isometric(target_velocity)	
-	
-	if base == null:
-		velocity = target_velocity
-		move_and_slide()
-	else:
-		base.velocity = target_velocity
-		base.move_and_slide()
-		
-		position.x = base.position.x
-		position.y = base.position.y + 1
-		position.z = base.position.z
-		
-func cartesian_to_isometric(cartesian):
+func cartesian_to_isometric(cartesian: Vector3) -> Vector3:
 	# Rotation by 45 degrees clockwise: https://en.wikipedia.org/wiki/Rotation_matrix
 	var iso_x = cartesian.x * COS_45  - cartesian.z * SIN_45
 	var iso_z = cartesian.x * SIN_45 + cartesian.z * COS_45
 	
 	return Vector3(iso_x, cartesian.y, iso_z)
 		
+func handle_stacking() -> void:
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+		var collider = collision.get_collider()
+		
+		# If the collision is unknown or the base of the stack
+		if collider == null or collider == base or collider == prev_base:
+			continue
+
+		# If the collision is with the world, player hit the ground and can now become a stack again
+		if collider.is_in_group("world"):
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				prev_base = null
+				
+				break
+			
+		# If the collider is with an npc, stack
+		if collider.is_in_group("npc"):
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# BUG: Setting position of player directly makes it so their collider is ignored
+				position.x = collider.position.x
+				position.y = collider.position.y + 1
+				position.z = collider.position.z
+				base = collider
+				
+				break	
 		
